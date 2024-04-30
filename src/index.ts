91e68c7f90c589e2a0426d2bad3e98a7e885675f
Kanji Miyatani <kanji09012458@gmail.com>
2024-04-30 20:46:37 +0900

    add nodemon

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request } from 'express';
+
+const app : express.Express = express();
+app.get("/", (req : express.Request,res :express.Response) => {
+    console.log("request listened.")
+    res.send("aaa");
+})
+app.listen(3000 ,() => {
+    console.log("Start on port 3000.");
+   
+});
