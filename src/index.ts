2739d533890901ac832c801ef15fac4e87241bf0
Kanji Miyatani <kanji09012458@gmail.com>
2024-06-09 22:25:12 +0900

    Add Loigin modules

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ app.get("/", (req : express.Request,res :express.Response) => {
     console.log("request listened.")
     res.send("aaa");
 })
+app.get("/login", (req : express.Request,res :express.Response) => {
+   
+})
 app.listen(3000 ,() => {
     console.log("Start on port 3000.");
    
